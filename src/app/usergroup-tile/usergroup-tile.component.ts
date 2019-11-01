6f15abca4608ec91a3d3af013599b116538c5f70
sean drexler <seand@imprezzio.com>
2019-11-01 15:19:17 -0700

    updates for resizing

--- src/app/usergroup-tile/usergroup-tile.component.ts
+++ src/app/usergroup-tile/usergroup-tile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-usergroup-tile',
@@ -7,15 +9,51 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./usergroup-tile.component.scss']
 })
 export class UsergroupTileComponent implements OnInit {
-  usergroupName = 'My Awesome Team';
-  users: Array<string> = [];
-
+  isOpen = false;
+  userGroupName = 'My Awesome Team';
   myControl = new FormControl();
-  options: string[] = ['One', 'Two', 'Three'];
+  allUsers = [
+    'Al Jenkins', 'Freddie From', 'Ethan Jones', 'Gina Hurley',
+    'Kevin Armstrong', 'Bobbie Schroeder', 'Anne Batdorf', 'Ford Wright',
+    'John Smith', 'Sam Wellington', 'Jane Doe', 'Fred Rogers',
+    'John Stanley', 'Samuel Wellington', 'Janet Doe', 'Fredrick Rogers',
+    'Jonah Smith', 'Sam Well', 'Joyce Done', 'Fredda Robbert',
+    'Steve Blitz', 'Forrest Tracker', 'Mercedes Liberty', 'Roger Zapp',
+    'George Ronalds', 'Patty Snicker', 'Lake Johnson', 'Brian Davis'
+  ];
+  groupUsers = [
+    'John Smith', 'Sam Wellington', 'Jane Doe', 'Fred Rogers',
+    'John Stanley', 'Samuel Wellington', 'Janet Doe', 'Fredrick Rogers',
+    'Jonah Smith', 'Sam Well', 'Joyce Done', 'Fredda Robbert',
+    'Steve Blitz', 'Forrest Tracker', 'Mercedes Liberty', 'Roger Zapp',
+    'George Ronalds', 'Patty Snicker', 'Lake Johnson', 'Brian Davis'
+  ];
+  filteredUsers: Observable<string[]>;
 
   constructor() { }
 
   ngOnInit() {
+    this.groupUsers.sort();
+    this.filteredUsers = this.myControl.valueChanges
+      .pipe(
+        startWith(''),
+        map(value => this._filter(value))
+      );
+  }
+
+  onClickAdd(event: Event): void {
+    const user = this.myControl.value;
+    this.groupUsers.push(user);
+    this.groupUsers.sort();
+    // TODO: clear the text input
+  }
+
+  onClickOpen(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  private _filter(value: string): string[] {
+    return this.allUsers.filter(user => user.toLowerCase().includes(value.toLowerCase()) && !this.groupUsers.includes(user));
   }
 
 }
