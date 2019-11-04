b4448995cd18a633978f5c0515b6355dbf77b60f
sean drexler <seand@imprezzio.com>
2019-11-04 15:11:16 -0800

    convert to string array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'usergroup-app';
-  count = [0,1,2,3,4,5,6,7,8,9];
+  groups = [
+    'Group 0',
+    'Group 1',
+    'Group 2',
+    'Group 3',
+    'Group 4',
+    'Group 5',
+    'Group 6',
+    'Group 7',
+    'Group 8',
+    'Group 9'
+  ];
 }
